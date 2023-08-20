f8538759163b71c3d9561674a178ba2667fe7bb6
Rohan Kumar <96805095+rohankumar009@users.noreply.github.com>
2023-08-19 23:30:06 -0600

    Added BinaryTreeRightSideView.py

---
+++ BinaryTreeRightSideView.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+
+            for i in range(level_size):
+                node = queue.popleft()
+
+                # Add the rightmost node value of each level to the result list
+                if i == level_size - 1:
+                    result.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return result
